85de0939a67417cbdb9df9ce1e4da81a38fd80bc
Oladon <oladon+git@gmail.com>
2019-12-28 01:56:42 -0700

    Don't include untracked files

--- build.sh
+++ build.sh
@@ -2,18 +2,24 @@
 
 cd src
 
+UNTRACKED=`git status -s | grep '??' | awk -F'\?\? ' '{ print $2; }'`;
+
 ## Firefox
 VERSION=`grep '"version"' manifest.json.firefox | grep -oE '[0-9.]+'`;
+FILENAME=pct-firefox-${VERSION}.xpi
+rm $FILENAME
 cp manifest.json.firefox manifest.json
 cp skin/fonts.css.firefox skin/fonts.css
-zip -r pct-firefox-${VERSION}.xpi content/ skin/ manifest.json
-zip -d pct-firefox-${VERSION}.xpi skin/fonts.css.firefox skin/fonts.css.chrome
+zip -r $FILENAME content/ skin/ manifest.json
+zip -d $FILENAME skin/fonts.css.firefox skin/fonts.css.chrome $UNTRACKED
 
 ## Chrome
 VERSION=`grep '"version"' manifest.json.chrome | grep -oE '[0-9.]+'`;
+FILENAME=pct-chrome-${VERSION}.zip
+rm $FILENAME
 cp manifest.json.chrome manifest.json
 cp skin/fonts.css.chrome skin/fonts.css
-zip -r pct-chrome-${VERSION}.zip content/ skin/ manifest.json
-zip -d pct-chrome-${VERSION}.zip skin/fonts.css.firefox skin/fonts.css.chrome
+zip -r $FILENAME content/ skin/ manifest.json
+zip -d $FILENAME skin/fonts.css.firefox skin/fonts.css.chrome $UNTRACKED
 
 echo 'Done!'
